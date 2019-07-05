19cb4ed34451a3dd23d514708f35ec8a291ba9ed
malu0908 <40873568+malu0908@users.noreply.github.com>
2019-07-05 12:56:10 -0300

    Update Novo_novo.java

--- Novo_novo.java
+++ Novo_novo.java
@@ -1,150 +1,166 @@
+package hopcroft.karp;
+
 import java.io.*;
 import java.util.*;
 
-public class Teste {
-	private final int NIL = 0;
-	private final int INF = Integer.MAX_VALUE;
-	private ArrayList<Integer>[] Adj;
-	private int qtd = 12;
-	private int[] Group1 = new int[qtd / 2];
-	private int[] Group2 = new int[qtd / 2];
-	private int[] Dist = new int[qtd/2];
-	private int[][] m = new int[qtd][qtd];
+public class HopcroftKarp {
 
-	public boolean BFS() {
+    private ArrayList<Integer>[] Adj;
+    private int qtd = 12;
+    private int[] Group1 = new int[qtd / 2];
+    private int[] Group2 = new int[qtd / 2];
+    private int[] Dist = new int[qtd / 2];
+    private int[][] m = new int[qtd][qtd];
 
-		int k = INF;
-		Queue<Integer> queue = new LinkedList<Integer>();
+    public boolean BFS() {
 
-		for (int v = 0; v < Group1.length; v++)
-			if (Group1[v] == v) {
-				Dist[v] = 0;
-				queue.add(v);
-			} else
-				Dist[v] = INF;
+        int k = Integer.MAX_VALUE;
+        Queue<Integer> queue = new LinkedList<Integer>();
 
-		while (!queue.isEmpty()) {
+        for (int v = 0; v < Group1.length; v++) {
+            if (Group1[v] == v) {
+                Dist[v] = 0;
+                queue.add(v);
+            } else {
+                Dist[v] = Integer.MAX_VALUE;
+            }
+        }
 
-			int u = queue.poll();
+        while (!queue.isEmpty()) {
 
-			if (Dist[u] < k)
-				for (int v : Adj[u])
-					if (v == Group2[v] && k == INF)
-						k = Dist[u] + 1;
-					else if (Dist[Group2[u]] == INF) {
-						Dist[Group2[v]] = Dist[v] + 1;
-						queue.add(Group2[v]);
-					}
-		}
+            int u = queue.poll();
 
-		return k != INF;
-	}
+            if (Dist[u] < k) {
+                for (int v : Adj[u]) {
+                    if (v == Group2[v] && k == Integer.MAX_VALUE) {
+                        k = Dist[u] + 1;
+                    } else if (Dist[Group2[v]] == Integer.MAX_VALUE) {
+                        Dist[Group2[v]] = Dist[u] + 1;
+                        Dist[Group2[u]] = Dist[u] + 1;
+                        queue.add(Group2[v]);
+                    }
+                }
+            }
+        }
 
-	public boolean DFS(int v) {
-		for (int u : Adj[v]) {
-			if (Group2[u] == u) {
-				Group1[v] = u;
-				Group2[u] = v;
-				return true;
-			}
-			if (Dist[Group2[u]] == Dist[v] + 1) {
-				if (DFS(Group2[v]) == true) {
-					Group1[v] = u;
-					Group2[u] = v;
-					return true;
-				}
-			}
-		}
-		Dist[v] = INF;
-		return false;
-	}
+        return k != Integer.MAX_VALUE;
+    }
 
-	public void HopcroftKarp() {
-		Dist = new int[qtd];
-		boolean resp = BFS();
-		while (resp == true) {
-			for (int v = 0; v < Group1.length; v++)
-				DFS(v);
-			resp = BFS();
-		}
-	}
+    public boolean DFS(int v) {
+        for (int u : Adj[v]) {
+            if (Group2[u] == u) {
+                Group1[v] = u;
+                Group1[u] = v;
 
-	public void makeGraph() throws IOException {
-		if (Adj == null) {
-			FileReader arq = new FileReader("/home/2018.1.08.015/Desktop/Hopcroft_Karp/src/input.txt");
+                Group2[u] = v;
+                Group2[u] = v;
+                return true;
+            }
+            if (Dist[Group2[u]] == Dist[v] + 1) {
+                if (DFS(Group2[v]) == true) {
+                    Group1[v] = u;
+                    Group1[u] = v;
 
-			BufferedReader lerArq = new BufferedReader(arq);
-			String qtd = lerArq.readLine();
+                    Group2[u] = v;
+                    Group2[u] = v;
+                    return true;
+                }
+            }
+        }
+        Dist[v] = Integer.MAX_VALUE;
+        return false;
+    }
 
-			Adj = new ArrayList[Integer.parseInt(qtd)];
+    public void HopcroftKarp() {
+        Dist = new int[qtd];
+        boolean resp = BFS();
+        while (resp == true) {
+            for (int v = 0; v < Group1.length; v++) {
+                DFS(v);
+            }
+            resp = BFS();
+        }
+    }
 
-			String linha = lerArq.readLine();
-			int i = 0;
-			while (linha != null) {
-				Adj[i] = new ArrayList<Integer>();
-				String[] info = linha.split(",");
-				for (int w = 0; w < Integer.parseInt(qtd); w++) {
-					if (Integer.parseInt(info[w]) == 1) {
-						if (w >= Integer.parseInt(qtd) / 2)
-							Adj[i].add(w - Integer.parseInt(qtd) / 2);
-						else
-							Adj[i].add(w);
-					}
-				}
-				linha = lerArq.readLine();
-				i++;
-			}
-			lerArq.close();
-		}
-	}
+    public void makeGraph() throws IOException {
+        if (Adj == null) {
+            FileReader arq = new FileReader("/home/2018.1.08.015/Desktop/Hopcroft_Karp/src/input.txt");
 
-	public void montaMatriz() {
-		Random r = new Random();
+            BufferedReader lerArq = new BufferedReader(arq);
+            String qtd = lerArq.readLine();
 
-		for (int i = 0; i < qtd / 2; i++) {
-			for (int j = qtd / 2; j < qtd; j++) {
-				if ((i + qtd / 2) != j) {
-					int a = r.nextInt(2);
-					if (m[i][j] == 0) {
-						m[i][j] = a;
-						if (a == 1)
-							m[j - qtd / 2][i + qtd / 2] = 1;
-					}
-				}
-			}
-		}
+            Adj = new ArrayList[Integer.parseInt(qtd)];
 
-		for (int i = qtd / 2; i < qtd; i++) {
-			for (int j = 0; j < qtd / 2; j++) {
-				m[i][j] = m[j][i];
-			}
-		}
-	}
+            String linha = lerArq.readLine();
+            int i = 0;
+            while (linha != null) {
+                Adj[i] = new ArrayList<Integer>();
+                String[] info = linha.split(",");
+                for (int w = 0; w < Integer.parseInt(qtd); w++) {
+                    if (Integer.parseInt(info[w]) == 1) {
+                        if (w >= Integer.parseInt(qtd) / 2) {
+                            Adj[i].add(w - Integer.parseInt(qtd) / 2);
+                        } else {
+                            Adj[i].add(w);
+                        }
+                    }
+                }
+                linha = lerArq.readLine();
+                i++;
+            }
+            lerArq.close();
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		Teste a = new Teste();
-		for (int i = 1; i < a.Group1.length; i++) {
-			a.Group1[i] = i;
-			a.Group2[i] = i;
-		}
+    public void montaMatriz() {
+        Random r = new Random();
 
-		boolean fazAlgoritmo = true;
-		if (fazAlgoritmo) {
-			a.makeGraph();
-			a.HopcroftKarp();
-			for (int i = 0; i < a.Group1.length; i++) {
-				System.out.println("(" + (i) + "," + (a.Group1[i]) + ")");
-			}
-		} else {
-			a.montaMatriz();
-			for (int i = 0; i < a.m.length; i++) {
-				for (int j = 0; j < a.m[0].length; j++) {
-					System.out.print(a.m[i][j]);
-					if ((j + 1) != a.qtd)
-						System.out.print(",");
-				}
-				System.out.println();
-			}
-		}
-	}
+        for (int i = 0; i < qtd / 2; i++) {
+            for (int j = qtd / 2; j < qtd; j++) {
+                if ((i + qtd / 2) != j) {
+                    int a = r.nextInt(2);
+                    if (m[i][j] == 0) {
+                        m[i][j] = a;
+                        if (a == 1) {
+                            m[j - qtd / 2][i + qtd / 2] = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = qtd / 2; i < qtd; i++) {
+            for (int j = 0; j < qtd / 2; j++) {
+                m[i][j] = m[j][i];
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        HopcroftKarp a = new HopcroftKarp();
+        for (int i = 0; i < a.Group1.length; i++) {
+            a.Group1[i] = i;
+            a.Group2[i] = i;
+        }
+
+        boolean fazAlgoritmo = true;
+        if (fazAlgoritmo) {
+            a.makeGraph();
+            a.HopcroftKarp();
+            for (int i = 0; i < a.Group1.length; i++) {
+                System.out.println("(" + (i) + "," + (a.Group1[i]) + ")");
+            }
+        } else {
+            a.montaMatriz();
+            for (int i = 0; i < a.m.length; i++) {
+                for (int j = 0; j < a.m[0].length; j++) {
+                    System.out.print(a.m[i][j]);
+                    if ((j + 1) != a.qtd) {
+                        System.out.print(",");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
 }
